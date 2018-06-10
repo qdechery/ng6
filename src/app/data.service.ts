da03f2cfe6c857fccc19c88f8959ec78982f0dca
qdechery <qdechery@gmail.com>
2018-06-09 17:20:16 -0700

    first commit; upload files

---
+++ src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(userId){
+  	if(!userId){
+  		return this.http.get('https://jsonplaceholder.typicode.com/users')
+  	} else {
+  	  	return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
+  	}
+  }
+
+  getPosts(){
+  	return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  }
+}
